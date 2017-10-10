1a1d77df11816bf46a67b0b9ed036b9d657325cf
Arshin Jain <arshinx@users.noreply.github.com>
2017-10-10 18:05:41 -0400

    Create and Push Marker

--- js/map.js
+++ js/map.js
@@ -31,7 +31,22 @@ function initMap() {
     // Get the position from the location array.
     var position = locations[i].location;
     var title = locations[i].title;
-    
+
+    // Create a marker per location, and put into markers array.
+    var marker = new google.maps.Marker({
+      map: map,
+      position: position,
+      title: title,
+      animation: google.maps.Animation.DROP,
+      id: i
+    });
+    // Push the marker to our array of markers.
+    markers.push(marker);
+    // Create an onclick event to open an infowindow at each marker.
+    marker.addListener('click', function() {
+      populateInfoWindow(this, largeInfowindow);
+    });
+
   }
 
 }

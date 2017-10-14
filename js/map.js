9f7d4495b79b82cb6b8b3c62c2766ae90e452292
Arshin Jain <arshinx@users.noreply.github.com>
2017-10-14 16:09:50 -0400

    Try to show coordinates in infowindow
    Need to be fixed.

--- js/map.js
+++ js/map.js
@@ -136,7 +136,7 @@ function initMap() {
     // Check to make sure the infowindow is not already opened on this marker.
     if (infowindow.marker != marker) {
       infowindow.marker = marker;
-      infowindow.setContent('<img src="https://www.city-journal.org/sites/cj/files/New-York.jpg" alt="' + marker.title + '"></img>' + '<div>' + marker.title + '</div>');
+      infowindow.setContent('<img src="https://www.city-journal.org/sites/cj/files/New-York.jpg" alt="' + marker.title + '"></img>' + '<div>' + marker.title + ' (' + marker.position.lat + ', ' + marker.position.lng + ')' + '</div>');
       infowindow.open(map, marker);
       // Make sure the marker property is cleared if the infowindow is closed.
       infowindow.addListener('closeclick',function(){

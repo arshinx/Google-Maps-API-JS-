b183079e0d290ae059e1ed900873ea1542636cbb
Arshin Jain <arshinx@users.noreply.github.com>
2017-10-19 14:08:35 -0400

    Filter listings/markers — hide

--- js/view model/view_model.js
+++ js/view model/view_model.js
@@ -16,6 +16,28 @@ function viewModel() {
     });
 
     // -- Filter List of Waterfalls -- //
+    self.filter = function() {
+
+      // Reset list and markers — display
+      self.waterfallsList.removeAll();
+      locations.forEach(function(value, key){
+        // Populate list
+        self.waterfallsList.push(locations[key]);
+        markers[key].setVisible(true);
+      });
+
+      // Filter listings/markers — hide
+      locations.forEach(function(value, key){
+        // Validate whether all elements are selected
+        if (self.state() !== 'Select All') {
+          if (value.state !== self.state()) {
+            // Update list
+            self.waterfallsList.remove(value);
+            markers[key].setVisible(false);
+          }
+        });
+
+    }
 
 };
 

d1e570a95ba91280e84897e8dfab4e54367fb0ba
Arshin Jain <arshinx@users.noreply.github.com>
2017-10-19 13:54:07 -0400

    Properties - locationsList

--- js/view model/view_model.js
+++ js/view model/view_model.js
@@ -1,7 +1,9 @@
 // View Model using Knockout
 function viewModel() {
-
     var self = this;
+
+    // Properties
+    self.locationsList = ko.observableArray();
 };
 
 ko.applyBindings(new viewModel());
